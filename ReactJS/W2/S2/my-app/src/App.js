eb34e78ab0bad0b9bb68bf58aa8e381b2ed6747f
dmn-05 <nnbdo45@gmail.com>
2025-09-04 01:18:05 +0700

    1:17 04/09/2025

--- ReactJS/W2/S2/my-app/src/App.js
+++ ReactJS/W2/S2/my-app/src/App.js
@@ -1,27 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [Arr, SetArr] = useState(["CDTH23A","CDTH23B","CDTH23C","CDTH23D","CDTH23E","CDTH23F","CDTH23H"])
-  return (
-    <>
-      <h1>Danh sach cac lop:</h1>
-      <ul>
-        {
-          Arr.map(x=>{
-            <li key={x}>
-              <p>{x}</p>
-              <button>Sua</button>
-              <button>Xoa</button>
-            </li>
-          })
-        }
-      </ul>
-      <input type='text'/>
-      <button>Them</button>
-    </>
-  );
+    const [Arr, setArr] = useState([
+        'CDTH23A',
+        'CDTH23B',
+        'CDTH23C',
+        'CDTH23D',
+        'CDTH23E',
+        'CDTH23F',
+        'CDTH23H',
+    ]);
+    const [addStr, setAddStr] = useState('');
+    const [oldStr, setOldStr] = useState('');
+    const [newStr, setNewStr] = useState('');
+    const handleAdd = function () {
+        setArr([...Arr, addStr]);
+        setAddStr('');
+    };
+    const handleDelete = function (del) {
+        setArr(Arr.filter((s) => s !== del));
+    };
+    const handleUpdate = function () {
+        setArr(Arr.map((s) => (s === oldStr ? newStr : s)));
+        setNewStr('');
+        setOldStr('');
+    };
+    return (
+        <>
+            <h1>Danh sách các lớp:</h1>
+            <ul>
+                {Arr.map((x) => {
+                    return (
+                        <li key={x}>
+                            {x === oldStr ? (
+                                <>
+                                    <input
+                                        value={newStr}
+                                        onChange={(e) =>
+                                            setNewStr(e.target.value)
+                                        }
+                                    />
+                                    <button onClick={handleUpdate}>Lưu</button>
+                                    <button
+                                        onClick={() => {
+                                            setOldStr('');
+                                        }}
+                                    >
+                                        Hủy
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <p>{x}</p>
+                                    <button
+                                        onClick={() => {
+                                            setOldStr(x);
+                                            setNewStr(x);
+                                        }}
+                                    >
+                                        Sửa
+                                    </button>
+                                    <button onClick={() => handleDelete(x)}>
+                                        Xóa
+                                    </button>
+                                </>
+                            )}
+                        </li>
+                    );
+                })}
+            </ul>
+            <input
+                type="text"
+                value={addStr}
+                onChange={(e) => setAddStr(e.target.value)}
+            />
+            <button disabled={!addStr.trim()} onClick={handleAdd}>
+                Thêm
+            </button>
+        </>
+    );
 }
 
 export default App;
